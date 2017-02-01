8464fd4df50d14c5b9a8adc79fcfd5cf94f51ae2
fuellgraf <fuellgraf@transporeon.com>
2017-02-01 16:32:50 +0100

    added data types

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,37 +1,32 @@
 #
 define systemd::service (
-    $execstart,
-    $servicename     = $name,
-    $description     = '',
-    $execstop        = undef,
-    $execreload      = undef,
-    $workingdir      = undef,
-    $restart         = 'always',
-    $restartsec      = undef,
-    $remainafterexit = false,
-    $user            = 'root',
-    $group           = 'root',
-    $type            = 'simple',
-    $defaultdeps     = true,
-    $requires        = [],
-    $conflicts       = [],
-    $wants           = [],
-    $after           = [],
-    $wantedby        = ['multi-user.target'],
-    $unit_path       = $systemd::params::unit_path,
+    String $execstart,
+    String $servicename      = $name,
+    String $description      = '',
+    String $execstop         = undef,
+    String $execreload       = undef,
+    String $workingdir       = undef,
+    String $restart          = 'always',
+    String $restartsec       = undef,
+    Boolean $remainafterexit = false,
+    String $user             = 'root',
+    String $group            = 'root',
+    String $type             = 'simple',
+    Boolean $defaultdeps     = true,
+    Array $requires          = [],
+    Array $conflicts         = [],
+    Array $wants             = [],
+    Array $after             = [],
+    Array $wantedby          = ['multi-user.target'],
+    String $unit_path        = $systemd::params::unit_path,
+    Array $confopts          = undef,
 ) {
 
     include systemd
 
     validate_re($restart, ['^always$', '^no$', '^on-(success|failure|abnormal|abort|watchdog)$'], "Not a supported restart type: ${restart}")
-    validate_bool($remainafterexit)
     validate_re($type, ['^simple$', '^forking$', '^oneshot$', '^dbus$', '^notify$', '^idle$'], "Not a supported type: ${type}")
-    validate_bool($defaultdeps)
-    validate_array($requires)
-    validate_array($conflicts)
-    validate_array($wants)
-    validate_array($after)
-    validate_array($wantedby)
+
 
     file { "${unit_path}/${servicename}.service":
         ensure  => 'present',
